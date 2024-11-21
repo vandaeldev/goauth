7f5902661bff9b51b8c1c0d64d29dcd1cf2d4889
Dominic van Dael <vandael.dev@gmail.com>
2024-11-21 11:09:35 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+  "fmt"
+  "net/http"
+)
+
+func main() {
+  http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "Hello World!")
+  })
+  fs := http.FileServer(http.Dir("static/"))
+  http.Handle("/static/", http.StripPrefix("/static/", fs))
+  http.ListenAndServe(":8080", nil)
+}
